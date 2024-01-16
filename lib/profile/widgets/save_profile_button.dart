3a9ad09f62916c580dcd3465438330b454c0b2d5
GeneralDido <dimpanouris@gmail.com>
2024-01-16 13:02:09 +0100

    refactor buttons using context from extensions

--- lib/profile/widgets/save_profile_button.dart
+++ lib/profile/widgets/save_profile_button.dart
@@ -1,27 +1,17 @@
-import 'package:flutter/material.dart' hide State;
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_example/profile/bloc.dart';
-import 'package:flutter_bloc_example/profile/models/errors.dart';
+import 'package:flutter_bloc_example/profile/extensions.dart';
 import 'package:flutter_bloc_example/profile/models/events.dart';
-import 'package:flutter_bloc_example/core/core.dart';
-import 'package:flutter_bloc_example/profile/models/profile.dart';
 
 class SaveProfileButton extends StatelessWidget {
   const SaveProfileButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ProfileService, State<Profile, ProfileErrorType>>(
-      builder: (context, state) {
-        final isStateLoading = state is LoadingState<Profile, ProfileErrorType>;
-
-        return ElevatedButton(
-          onPressed: isStateLoading
-              ? null
-              : () => context.read<ProfileService>().add(const SaveProfileEvent()),
-          child: const Text('Save'),
-        );
-      },
+    return ElevatedButton(
+      onPressed: context.profile.isLoading ? null : () => context.read<ProfileService>().add(const SaveProfileEvent()),
+      child: const Text('Save'),
     );
   }
 }
